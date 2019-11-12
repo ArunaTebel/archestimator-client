61ec790618b87604ceb543be7e85da3072b02113
ArunaTebel <arunatebel@hotmail.com>
2019-11-12 21:54:36 +0530

    fixing the backend api base url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const LOCAL_STORAGE_TOKEN_KEY = 'archestimator.token';
-export const BACKEND_API_BASE_URL = 'https://archestimator.herokuapp.com/';
+export const BACKEND_API_BASE_URL = 'https://archestimator.herokuapp.com';
 export const BACKEND_ESTIMATOR_API_URL = `${BACKEND_API_BASE_URL}/estimator/api`;
 export const PATHS_WITH_NO_NAVBAR = ['/login', '/logout', '/login/', '/logout/'];
 export const HANDSONTABLE_KEY = 'non-commercial-and-evaluation';
